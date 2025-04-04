7ab44ef38b39ab805e01a693e683a4aeb8c35cab
Mohamed_LABJAOUI <Mohamed.labjaoui07@gmail.com>
2025-04-04 15:36:44 +0000

    TP Docker Node.js is Done

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+require('dotenv').config();
+const express = require('express');
+const { Pool } = require('pg');
+
+const app = express();
+const port = 3000;
+
+// Connexion à PostgreSQL avec les variables d’environnement
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: 5432,
+});
+
+// Route pour récupérer les utilisateurs
+app.get('/users', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM users');
+    res.json(result.rows);
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+});
+
+app.listen(port, () => {
+  console.log(`🚀 Serveur démarré sur http://localhost:${port}`);
+});
